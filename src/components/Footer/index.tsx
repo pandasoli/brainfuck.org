4643b4aa4f6ad9c53b8622973963126c870eeb42
pandasoli <sadpandasoli@gmail.com>
2022-06-30 22:16:47 -0300

    [update] links to start with https

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -8,15 +8,15 @@ const Footer = () =>
     <Divider>
       <Title>Project repo</Title>
 
-      <a href='github.com/pandasoli/brainfuck.org'>GitHub</a>
+      <a href='https://github.com/pandasoli/brainfuck.org'>GitHub</a>
     </Divider>
 
     <Divider>
       <Title>Credits</Title>
 
-      <IconLabel icon='github' text='pandasoli' link='github.com/pandasoli'/>
-      <IconLabel icon='instagram' text='pandasoli.ofc' link='instagram.com/pandasoli.ofc'/>
-      <IconLabel icon='facebook' text='pandasoli.ofc' link='facebook.com/pandasoli.ofc'/>
+      <IconLabel icon='github' text='pandasoli' link='https://github.com/pandasoli'/>
+      <IconLabel icon='instagram' text='pandasoli.ofc' link='https://instagram.com/pandasoli.ofc'/>
+      <IconLabel icon='facebook' text='pandasoli.ofc' link='https://facebook.com/pandasoli.ofc'/>
     </Divider>
   </Container>
 

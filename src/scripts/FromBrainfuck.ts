1b094572ee0eda56c87980d6b0cfbf45d3351675
pandasoli <sadpandasoli@gmail.com>
2022-07-20 17:28:34 -0300

    [updated] new version

--- src/scripts/FromBrainfuck.ts
+++ src/scripts/FromBrainfuck.ts
@@ -1,103 +1,54 @@
 
-type Props = {
-	code: string[]
-	pause: boolean
-	warn: boolean
-	getChar: () => string
-	maxMem: 'auto' | number
+type ConfigProps = {
+	memory?: number[]
+	loops?: number[]
+	pointer?: number
+	result?: string
+	index?: number
 }
 
-
-function * From(_code?: string, _pause?: boolean, _warn?: boolean, _getChar?: () => string, _maxMem?: number) {
-	const props: Props = {
+export default function * bfinterpreter(_code?: string, _pause?: 'none' | 'before' | 'after' | 'both', _getChar?: () => string, _maxMem?: number, _config?: ConfigProps) {
+	const props = {
 		code: String(_code || '++++++++[>++++++++++<-]>.<+[>++++++++++<-]>+++++++.<+[>++++++++++<-]>+++.----------.---.!').split(''),
-		pause: Boolean(_pause),
-		warn: Boolean(_warn),
-		getChar: _getChar || ( () => prompt('enter a char:') || '' ),
-		maxMem: Number(_maxMem) > 0 ? Number(_maxMem)  : 'auto'
+		pause: ['none', 'before', 'after', 'both'].includes(_pause || '') ? _pause || '' : 'after',
+		getChar: _getChar || (() => prompt('enter a char:')),
+		maxMem: Number(_maxMem) || 'auto',
 	}
-	const memory = [0]
+	const memory = _config?.memory || [0]
 	const acceptedChars = '+-<>.,[]!'
-	const loops = []
-	const warnings = []
-	let pointer = 0
-	let result = ''
+	const loops = _config?.loops || []
+	let pointer = _config?.pointer || 0
+	let result = _config?.result || ''
 
-	for (let x = 0; x < props.code.length; x++) {
+	for (let x = _config?.index || 0; x < props.code.length; x++)
 		do {
 			const char = props.code[x]
 
 			if (acceptedChars.includes(char)) {
-				const resToReturn = { memory, pointer, result, index: x, char, loops, error: '', warning: '' }
+				if (['before', 'both'].includes(props.pause)) yield { memory, pointer, result, index: x, char, loops }
 
-				if (props.pause) { yield resToReturn }
-
-				if (char === '+') {
-					if (props.warn && memory[pointer] === 255) {
-						resToReturn.warning = 'Warning: memory passed 255.'
-						warnings.push({
-							index: x,
-							warning: resToReturn.warning
-						})
-
-						if (props.pause) {
-							yield resToReturn
-						}
-					}
-
-					memory[pointer] = memory[pointer] === 255 ? 0 : memory[pointer] + 1
-				}
-				else if (char === '-') {
-					if (props.warn && memory[pointer] === 0) {
-						resToReturn.warning = 'Warning: memory passed 0.'
-						warnings.push({
-							index: x,
-							warning: resToReturn.warning
-						})
-
-						if (props.pause) {
-							yield resToReturn
-						}
-					}
-
-					memory[pointer] = memory[pointer] === 0 ? 255 : memory[pointer] - 1
-				}
-
+				if (char === '+') memory[pointer] = memory[pointer] === 255 ? 0 : memory[pointer] + 1
+				else if (char === '-') memory[pointer] = (memory[pointer] === 0) ? 255 : memory[pointer] - 1
 				else if (char === '>') {
-					pointer++
+          pointer++
+          if (pointer === memory.length) {
+            if (props.maxMem === 'auto' || memory.length < props.maxMem)
+              memory.push(0)
+            else
+              throw `Error: memory cannot pass '${props.maxMem}' arrays.`
+          }
+        }
+				else if (char === '<') pointer--; if (pointer === -1) throw 'Error: pointer can not be less than 1'
+				else if (char === '.') result += String.fromCharCode( memory[pointer] )
+				else if (char === ',') memory[pointer] = (props.getChar as () => string)().charCodeAt(0)
+				else if (char === '!') return { memory, pointer, result, index: x, char, loops }
+				else if (char === '[') loops.unshift(x)
+				else if (char === ']') memory[pointer] === 0 ? loops.shift() : x = loops[0]
 
-					if (pointer === memory.length) {
-						if (props.maxMem === 'auto' || memory.length < props.maxMem) {
-							memory.push(0)
-						}
-						else {
-							memory.push(0)
-							resToReturn.error = `Error: memory cannot pass '${props.maxMem}' arrays.`
-							return resToReturn
-						}
-					}
-				}
-				else if (char === '<') {
-					pointer--
-
-					if (pointer === -1) {
-						resToReturn.error = 'Error: pointer cannot be less than 1.'
-						return { ...resToReturn, warnings }
-					}
-				}
-				else if (char === '.') { result += String.fromCharCode( memory[pointer] ) }
-				else if (char === ',') { memory[pointer] = props.getChar().charCodeAt(0) }
-				else if (char === '!') { return resToReturn }
-				else if (char === '[') { loops.unshift(x) }
-				else if (char === ']') { (memory[pointer] === 0) ? loops.shift() : x = loops[0] }
-
-				if (props.pause && !resToReturn.warning) { yield resToReturn }
+				if (['after', 'both'].includes(props.pause)) yield { memory, pointer, result, index: x, char, loops }
 			}
 
 		} while (loops.length > 0 && x++)
-	}
 
-	return { memory, pointer, result, loops, warnings }
+	return { memory, pointer, result, loops }
 }
-
-export default From
